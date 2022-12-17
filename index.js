267a8fbc3a79dda87f8c78e9a834e449101d0eea
Raahulwe2code <120568928+Raahulwe2code@users.noreply.github.com>
2022-12-17 12:36:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import 'dotenv/config'
+import bodyParser from 'body-parser'
+import { DbConfigs } from './src/Configs/DbConfigs.js';
+import userRouter from './src/Routers/UserRouter.js';
+
+import cors from 'cors'
+import AdminRouter from './src/Routers/AdminRouter.js';
+
+const PORT= process.env.PORT||3000
+const app=express();
+app.use(cors())
+
+ app.use(express.json())
+app.use(express.static('images'))
+DbConfigs();
+app.use(userRouter);
+app.use(AdminRouter);
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running at ${PORT}`)
+})
+
+
+
