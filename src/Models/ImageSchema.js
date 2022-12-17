e4add6dc78ed10205e2ee7763b45b285ade0d874
shivaniwe2code <103627287+shivaniwe2code@users.noreply.github.com>
2022-12-17 15:11:49 +0530

    nodePackage

--- src/Models/ImageSchema.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-  const ImageSchema= new  mongoose.Schema({
-    name:{type:String},
-    avatar:{type:String }
-
-})
- const ImageModel= mongoose.model('image', ImageSchema);
- export default ImageModel;
